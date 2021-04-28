e6b3b9259359c7f46fd5547df75c0a241b84dc4a
Thebechir <bechirnaouar2@gmail.com>
2021-04-28 13:42:43 +0100

    fixed errors in the back-end

--- routes/loginAPI.js
+++ routes/loginAPI.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -8,29 +8,27 @@ const Company = require('../models/companySchema');
 
 
 //login
-router.post('/login', async(req,res)=>{
-    const companyFound = await Company.findOne({email: req.body.email});
-    if(companyFound == null)
-    {
-        res.json({message: 'Please verify your email or password!'});
+router.post('/login', async (req, res) => {
+    const companyFound = await Company.findOne({ email: req.body.email });
+    if (companyFound == null) {
+        res.status(400).json({ message: 'Please verify your email or password!' });
     }
-    else{
-       const passwordEqual = await bcrypt.compare(req.body.password, companyFound.password);
-       if(passwordEqual == true)
-       {
-           // create a token
-           const tokenData = { 
+    else {
+        const passwordEqual = await bcrypt.compare(req.body.password, companyFound.password);
+        if (passwordEqual == true) {
+            // create a token
+            const tokenData = {
                 email: companyFound.email,
                 companyId: companyFound._id,
                 companyName: companyFound.companyName
-        };
-           const createdToken = jwt.sign(tokenData, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
-           res.json({message: 'Login successfully', token: createdToken});
-       }
-       else{
-           res.json({message:'Please verify your email or password!'});
-       }
+            };
+            const createdToken = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+            res.json({ message: 'Login successfully', token: createdToken });
+        }
+        else {
+            res.status(400).json({ message: 'Please verify your email or password!' });
+        }
     }
- });
+});
 
- module.exports = router;
+module.exports = router;
